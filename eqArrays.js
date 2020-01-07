65dbcddb8cdd0a7aa998934cb8d2906c10f7828b
Rangga <ranggaluksatrio@gmial.com>
2020-01-07 14:57:26 -0500

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+/* const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅✅✅ Asserstion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${actual} !== ${expected}`);
+}; */
+
+const eqArrays = (arr1, arr2) => {
+  value = true;
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      value = false;
+    }
+  }
+  console.log(value);
+};
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
