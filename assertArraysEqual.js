950aa9fb2798b72aabee81f357213e11c4c00a07
Rangga <ranggaluksatrio@gmial.com>
2020-01-07 16:21:37 -0500

    assertArraysEqual.js fix

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,17 @@
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
   value = true;
   for (i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       value = false;
     }
-  };
-  return value === true ? console.log(`✅✅✅ Asserstion Passed: ${arr1} === ${arr2}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${arr1} !== ${arr2}`);
+  }
+  return value;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  return actual === expected ? console.log(`✅✅✅ Asserstion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${actual} !== ${expected}`);
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
