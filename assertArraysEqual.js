57480f1d13071d2e4d380e4899af11c2c05e471e
Rangga <ranggaluksatrio@gmial.com>
2020-01-13 13:34:34 -0500

    assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,13 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-  value = true;
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      value = false;
-    }
-  }
-  return value;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (actual, expected) => {
   return eqArrays(actual, expected) ? console.log(`✅✅✅ Asserstion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${actual} !== ${expected}`);
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+
+module.exports = assertArraysEqual;
+
