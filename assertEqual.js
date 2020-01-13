330b480550ccd83c869c3860a8275b1e08afcd20
Rangga <ranggaluksatrio@gmial.com>
2020-01-13 12:17:28 -0500

    assertEqual modularization

--- assertEqual.js
+++ assertEqual.js
@@ -2,5 +2,8 @@ const assertEqual = function(actual, expected) {
   return actual === expected ? console.log(`✅✅✅ Asserstion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,12 +1,11 @@
-
-
-const assertEqual = function(actual, expected) {
-  return actual === expected ? console.log(`✅✅✅ Asserstion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Asserstion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
